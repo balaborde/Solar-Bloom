e2143827b3243502627fb470701dd1d7d30875fd
Maxime Bodin <maxime.bodin@mailo.com>
2025-01-29 11:19:46 +0100

    Working draggable and hoverable cards.

--- card_manager.gd
+++ card_manager.gd
@@ -3,6 +3,7 @@ extends Node2D
 const COLLISION_MASK_CARD = 1
 var card_being_dragged
 var screen_size
+var is_hovering_on_card
 
 
 
@@ -25,11 +26,19 @@ func _input(event):
 			print("left click")
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card
+				start_drag(card)
 		else:
-			card_being_dragged = null
+			finish_drag()
 			print("left click released")
-			
+	
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(1, 1)
+
+func finish_drag():
+	if card_being_dragged:
+		card_being_dragged.scale = Vector2(1.05, 1.05)
+		card_being_dragged = null
 
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
@@ -40,5 +49,50 @@ func raycast_check_for_card():
 	var result = space_state.intersect_point(parameters)
 	print (result)
 	if result.size() > 0:
-		return result[0].collider.get_parent()
+		# return result[0].collider.get_parent()
+		return get_card_with_highest_z_index(result)
 	return null
+
+
+func get_card_with_highest_z_index(cards):
+	# Assume the first card in cards array has the highest z index.
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	# Loop through the rest of the cards checking for a higher z index.
+	for i in range(1, cards.size()):
+		var current_card = cards[1].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
+
+	
+func connect_card_signals(card): 
+	card.connect("hovered", on_card_hovered)
+	card.connect("hovered_off", on_card_hovered_off)
+
+
+func on_card_hovered(card):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+	highlight_card(card, true)
+
+func on_card_hovered_off(card):
+	if !card_being_dragged:
+		highlight_card(card, false)
+		# Check if hovered off card straight on to another card
+		var new_card_hovered = raycast_check_for_card()
+		if new_card_hovered:
+			highlight_card(new_card_hovered, true)
+		else:
+			is_hovering_on_card = false
+
+
+func highlight_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1, 1)
+		card.z_index = 1
