ecc80c8cd450e4fdae4221b7fd59bd0ff7c8a56e
Maxime Bodin <maxime.bodin@mailo.com>
2025-01-29 09:36:51 +0100

    working grid with adjusted camera pos.

--- plateau.gd
+++ plateau.gd
@@ -2,12 +2,13 @@ extends Node3D
 
 var case_scene = preload("res://case.tscn")
 var grid_size = 10
-var spacing = 2.0
+var spacing = .2
+var clamp = .1
 
 func _ready():
 	for x in range(grid_size):
 		for z in range(grid_size):
-			var pos = Vector3(x * spacing, 0, z * spacing)
+			var pos = Vector3(x * spacing + clamp, 0, z * spacing + clamp)
 			instCase(pos)
 
 func instCase(pos):
