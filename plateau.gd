cc78797bfe6b0a8c53cc391a53f91f70548f21f1
Maxime Bodin <maxime.bodin@mailo.com>
2025-01-29 09:30:24 +0100

    Adding grid of cases and camera.

--- plateau.gd
+++ plateau.gd
@@ -1,14 +1,16 @@
 extends Node3D
 
-
-var case = preload("res://case.tscn")
+var case_scene = preload("res://case.tscn")
+var grid_size = 10
+var spacing = 2.0
 
 func _ready():
-	instCase(pos)
-
-
-
+	for x in range(grid_size):
+		for z in range(grid_size):
+			var pos = Vector3(x * spacing, 0, z * spacing)
+			instCase(pos)
 
 func instCase(pos):
-	var case = case.instantiate()
-	add_child(case)
+	var new_case = case_scene.instantiate()
+	new_case.position = pos
+	add_child(new_case)
