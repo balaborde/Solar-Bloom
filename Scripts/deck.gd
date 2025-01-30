4acbc53cedc81a97e9d681458f949527f66890d7
Maxime Bodin <maxime.bodin@mailo.com>
2025-01-30 10:10:17 +0100

    5 done

--- Scripts/deck.gd
+++ Scripts/deck.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-const HAND_COUNT = 4
 const CARD_SCENE_PATH = "res://Scenes/card.tscn"
 
 var player_hand_reference
@@ -9,13 +8,22 @@ var player_deck = ["Éolienne", "Éolienne", "Éolienne"]
 
 func _ready():
 	player_hand_reference = $"../PlayerHand"
+	$RichTextLabel.text = str(player_deck.size())
 		
-	var card_scene = preload(CARD_SCENE_PATH)
-	for i in range(HAND_COUNT):
-		var new_card = card_scene.instantiate()
-		$"../CardManager".add_child(new_card)
-		new_card.name = "Card " + str(i) 
-		player_hand_reference.add_card_to_hand(new_card)
-	
+
 func draw_card():
-	print("drawing card")
+	var card_drawn = player_deck[0]
+	player_deck.erase(card_drawn)
+	
+	# If player drew the last card in the deck, disable the deck
+	if player_deck.size() == 0:
+		$Area2D/CollisionShape2D.disabled = true
+		$Sprite2D.visible = false
+		$RichTextLabel.visible = false
+	
+	$RichTextLabel.text = str(player_deck.size())
+	var card_scene = preload(CARD_SCENE_PATH)
+	var new_card = card_scene.instantiate()
+	$"../CardManager".add_child(new_card)
+	new_card.name = "Card " + str(player_deck.size()) 
+	player_hand_reference.add_card_to_hand(new_card)
